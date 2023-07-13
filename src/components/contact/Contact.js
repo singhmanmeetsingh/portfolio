3a9c6aeeb6d86b0f9a4fa2ae47a9326fe8562977
singhmanmeetsingh <singhmanmeet523@gmail.com>
2023-07-13 18:09:57 -0400

    stack section added

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,11 +1,15 @@
 import React, { useRef } from "react";
 import "./Contact.css";
 import emailjs from "@emailjs/browser";
+import Git from "../../assets/git.svg";
+import Link from "../../assets/link.svg";
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
 function Contact() {
   const form = useRef();
 
   const sendEmail = (e) => {
-    debugger;
     e.preventDefault();
 
     emailjs
@@ -18,15 +22,22 @@ function Contact() {
       .then(
         (result) => {
           console.log(result.text);
+          toast.success("Email Sent !", {
+            position: toast.POSITION.TOP_CENTER
+          });
         },
         (error) => {
           console.log(error.text);
+          toast.error("Error !", {
+            position: toast.POSITION.TOP_CENTER,
+          });
         }
       );
   };
   return (
     <div className="contact-container">
       <div className="contact-padding">
+        <h1>Contact And Socials</h1>
         <form ref={form} onSubmit={sendEmail} className="contact-form">
           <label className="contact-label">Name</label>
           <input className="contact-input" type="text" name="user_name" />
@@ -34,9 +45,22 @@ function Contact() {
           <input className="contact-input" type="email" name="user_email" />
           <label className="contact-label">Message</label>
           <textarea className="contact-textarea" name="message" />
-          <input className="contact-input contact-button" type="submit" value="Send" />
+          <input
+            className="contact-input contact-button"
+            type="submit"
+            value="Send"
+          />
         </form>
+        <div className="contact-socials">
+            <div className="contact-section">Tel : 705-241-1972</div>
+            <div className="contact-section">
+                <a href="https://github.com/singhmanmeetsingh" target="_blank"><img src={Git} alt="github"/></a>
+                <a href="https://www.linkedin.com/in/manmeet-singh-788ab2a4/" target="_blank"><img src={Link} alt="linkedin"/></a>
+            </div>
+            <div className="contact-section">Email : workmanmeet7@gmail.com</div>
+        </div>
       </div>
+      <ToastContainer />
     </div>
   );
 }
--- src/components/stack/Stack.js
+++ src/components/stack/Stack.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import "./Stack.css";
+import Javascript from "../../assets/javascript.svg";
+import TypeScript from "../../assets/typescript.svg";
+import CSharp from "../../assets/CSharp.svg";
+import ReactImg from "../../assets/react.svg";
+import NextImg from "../../assets/next.svg";
+import ReduxImg from "../../assets/redux.svg";
+import Sql from "../../assets/sql.svg";
+import Mongo from "../../assets/mongo.svg";
+import Rust from "../../assets/rust.svg";
+import Node from "../../assets/node.svg";
 
 function Stack() {
+  // these images from svgrepo
+  let items = [
+    {imgsrc: Javascript , imgTitle: "Javascript"},
+    {imgsrc: TypeScript, imgTitle: "TypeScript"},
+    {imgsrc: CSharp, imgTitle: "CSharp"},
+    {imgsrc: ReactImg, imgTitle: "Reactjs"},
+    {imgsrc: NextImg, imgTitle: "Nextjs"},
+    {imgsrc: ReduxImg , imgTitle: "Redux"},
+    {imgsrc: Sql, imgTitle: "SQL"},
+    {imgsrc: Mongo, imgTitle: "MongoDB"},
+    {imgsrc: Rust, imgTitle: "Rust"},
+    {imgsrc: Node, imgTitle: "Node js"}
+
+  ]
   return (
     <div className='stack'>
       <div className='stack-pad'>
         <h2 className='stack-heading'>My Stack...</h2>
         <div className='stack-container stack-heading'>
             <div className='stack-block'>
-                this is stack block
+                {
+                  items.map((obj) => <img src={obj.imgsrc} title={obj.imgTitle}/>)
+                }
             </div>
         </div>
       </div>
